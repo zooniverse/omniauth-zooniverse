f2a4bb95429f3776de00404bccf42ea10c3616b8
Stuart <stuart@zooniverse.org>
2015-05-06 14:10:34 -0500

    adding public to the default requested scopes

--- lib/omniauth/strategies/zooniverse.rb
+++ lib/omniauth/strategies/zooniverse.rb
@@ -6,12 +6,32 @@ module OmniAuth
       # change the class name and the :name option to match your application name
       option :name, :zooniverse
 
+      BASE_SCOPES = %w[user public]
+      DEFAULT_SCOPE = "user,public"
+
       option :client_options, {
         :site => "https://panoptes-staging.zooniverse.org",
         :authorize_url => "/oauth/authorize",
+        :scope => "user, public"
       }
 
       uid { raw_info["id"] }
+
+      option :authorize_options, [:user, :public]
+
+
+      def authorize_params
+        super.tap do |params|
+          options[:authorize_options].each do |k|
+            params[k] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
+          end
+
+          raw_scope = params[:scope] || DEFAULT_SCOPE
+          scope_list = raw_scope.split(" ").map {|item| item.split(",")}.flatten
+          scope_list.map! { |s| s =~ /^https?:\/\// || BASE_SCOPES.include?(s) ? s : "#{BASE_SCOPE_URL}#{s}" }
+          params[:scope] = scope_list.join(" ")
+        end
+      end
 
       info do
         {
@@ -19,6 +39,7 @@ module OmniAuth
           :name  => raw_info["display_name"]
         }
       end
+
 
       def raw_info
         raw = JSON.parse(access_token.get('/api/me', headers: {'Accept' => "application/vnd.api+json; version=1"}).body)
